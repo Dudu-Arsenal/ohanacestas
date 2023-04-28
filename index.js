07f5697051084ce32c0b47c82ad5966676aebe5e
Dudu <111618029+Dudu-Arsenal@users.noreply.github.com>
2023-04-28 20:00:50 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let count = 1;
+setInterval( function(){
+    nextImage();
+    console.log("A")
+}, 5000)
+function nextImage(){
+    count++;
+    if(count>4){
+        count = 1;
+    }
+    document.getElementById("radio"+count).checked = true;
+}
