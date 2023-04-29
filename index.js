42777adcc123687ff53e97d55f0ceceb17cd11e2
Dudu <edudocross@gmail.com>
2023-04-28 22:53:02 -0300

    add

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let count = 1;
 setInterval( function(){
     nextImage();
-    console.log("A")
 }, 5000)
 function nextImage(){
     count++;
@@ -10,3 +9,7 @@ function nextImage(){
     }
     document.getElementById("radio"+count).checked = true;
 }
+
+document.querySelector('#seta_up').addEventListener('click', () => {
+    window.scrollTo(top)
+})
